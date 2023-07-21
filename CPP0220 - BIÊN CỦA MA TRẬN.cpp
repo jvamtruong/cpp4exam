427911b55b8367bf81600bc74ba400508550f33f
Nam <jvamtruong@gmail.com>
2023-07-21 07:49:05 +0700

    add CPP0220

---
+++ CPP0220 - BIÊN CỦA MA TRẬN.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n + 3][n + 3];
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0 ; j < n; j++) {
+                cin >> a[i][j];
+            }
+        }
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0 ; j < n; j++) {
+                if (0 < i && i < n - 1 && 0 < j && j < n - 1) cout << " " << " ";
+                else cout << a[i][j] << " ";
+                if (j == n - 1) cout << endl;
+            }
+        }
+    }
+}
